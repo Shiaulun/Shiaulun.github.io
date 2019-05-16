73b68e87e17924ad876a1d2264c890eb7078b523
WEN-SHIH-HUNG <howard1222945@gmail.com>
2019-05-16 22:25:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,13 @@ const resultOutput = document.getElementById('result')
 function login() {
  resultOutput.style.color = '#00f'
  resultOutput.innerText = 'Login..'
-
  setTimeout(function() {
   const accountValue = accountInput.value
     passwordValue = passwordInput.value
   if (accountValue === 's123456' &&
     passwordValue === '123456') {
    resultOutput.style.color = '#0f0'
-   
-   resultOutput.innerText = 'Hi, welcome Shiaulun'
- 
+   resultOutput.innerText = 'Hi, welcome JS 20 Min'
   } else {
    resultOutput.style.color = '#f00'
    resultOutput.innerText = '請檢查帳號密碼是否正確'
